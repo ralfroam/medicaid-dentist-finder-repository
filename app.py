af9b391b75c80a4bb7e6bdaf342695e716072187
ralfroam <ralfroam@hotmail.com>
2025-10-02 16:18:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+    
+
+
+
